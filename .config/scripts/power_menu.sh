655e8fcb62d82cfb5b117c394d14d7adf1b9e3a4
christian80gabi <christian-gabriel.nana@outlook.com>
2024-10-28 15:46:40 +0100

    Initial commit. Hyprland with catpuccin mocha theme color.

---
+++ .config/scripts/power_menu.sh
@@ -0,0 +1,22 @@
+!/usr/bin/env bash
+
+CONFIG="$HOME/.config/wofi/theme/config/config"
+STYLE="$HOME/.config/wofi/theme/src/mocha/style.css"
+
+options=" Poweroff\n Reboot\n Suspend\n Lock\n Logout"
+
+selected=$( echo -e $options | wofi --conf "${CONFIG}" --style "${STYLE}" -i --dmenu | awk '{print tolower($2)}' )
+
+case $selected in 
+  poweroff)
+    exec systemctl $selected -i;;
+  reboot)
+    exec systemctl $selected;;
+  suspend)
+    exec systemctl $selected;;
+  lock)
+    hyprlock;;
+  logout)
+    hyprctl dispatch exit;;
+esac
+
